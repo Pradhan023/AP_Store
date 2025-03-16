43c734ce724d3d30342696dbf4538938c1002da9
Anish Pradhan <anishpradha5523@gmail.com>
2025-03-16 15:59:44 +0530

    changes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -4,16 +4,14 @@ import { redis } from "../lib/redis.js";
 
 
 const generateToken = (userId) => {
-  const accessSecret = process.env.ACCESS_SECRET_KEY;
-  const refreshSecret = process.env.REFRESH_SECRET_KEY;
 
   if (!accessSecret || !refreshSecret) {
     throw new Error("Missing JWT secret keys in environment variables");
   }
-  const accessToken = jwt.sign({ userId }, accessSecret, {
+  const accessToken = jwt.sign({ userId }, process.env?.ACCESS_SECRET_KEY, {
     expiresIn: "15m",
   });
-  const refreshToken = jwt.sign({ userId }, refreshSecret, {
+  const refreshToken = jwt.sign({ userId }, process.env?.REFRESH_SECRET_KEY, {
     expiresIn: "7d",
   });
 
